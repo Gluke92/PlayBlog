a5a4155af75ae14a8a047330a5ef1692d696816c
George Luke <george.luke@george.luke>
2022-03-01 14:58:01 -0800

    Navbar and Home components

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+        <h1>The Dojo</h1>
+        <div>
+            <a href="/">Home</a>
+            <a href="/Create">Create</a>
+            {/* <a href="/">Home</a> */}
+        </div>
+    </nav>);
+}
+ 
+export default Navbar;
