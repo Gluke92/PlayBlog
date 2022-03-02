680ccaacfc95749322f715108d588383d662c900
George Luke <george.luke@george.luke>
2022-03-02 10:50:10 -0800

    update hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,30 @@
+import {useState} from 'react';
+
 const Home = () => {
+
+    // let name = 'Devorah';
+    const [name, setName] = useState('Devorah');
+    const [age, setAge] = useState(1.8);
+
+
+    const handleClick = (name) => {
+        if (name === 'Daddy') {
+            setName('Devorah');
+            setAge(1.8);
+        }
+        else {
+        setName('Daddy');
+        setAge(29);
+        }
+        // name = 'Daddy';
+    }
+
     return (  
         <div className="home">
             <h1>Homepage</h1>
+            <button onClick={() => handleClick(name)}>Click Me</button>
+            <h2>{name}</h2>
+            <p> {name} is {age} years old</p>
         </div>
     );
 }
